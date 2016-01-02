197239103c27644a3e92ec0334b8fe99a467a837
aldo lazuardi <aldooo_11@yahoo.com>
2016-01-02 16:01:40 +0700

    Initial Commit

---
+++ Pokedex-Practice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pokedex-Practice
+//
+//  Created by Aldo on 1/2/16.
+//  Copyright © 2016 Playsure. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
